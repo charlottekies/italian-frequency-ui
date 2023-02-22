93360e8a5a91ff7ef747b028124c415bbc9ab04b
Charlotte Kies <chopekies@gmail.com>
2023-02-22 18:04:18 -0500

    modified vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/hello-world/" : "/",
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
