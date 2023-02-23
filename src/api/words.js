0b9be1cd407a1a766eeafc026be56f2ad811dcce
Charlotte Kies <chopekies@gmail.com>
2023-02-22 19:32:30 -0500

    removed secret reference

--- src/api/words.js
+++ src/api/words.js
@@ -7,10 +7,7 @@ const BASE_URL =
     ? `${secret.BASE_URL}`
     : process.env.API_BASE_URL;
 
-const BASE_URL2 =
-  process.env.NODE_ENV === "production" && secret.BASE_URL
-    ? secret.BASE_URL
-    : process.env.API_BASE_URL;
+const BASE_URL2 = process.env.API_BASE_URL;
 /*eslint-enable */
 
 export function getWords() {
